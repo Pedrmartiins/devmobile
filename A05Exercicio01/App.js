5a781e4c9e97512c28ef64b29fe8af7025b9005e
Pedrmartiins <pedroenriquellopes2011@gmail.com>
2025-02-14 22:01:33 -0300

    terminar o projeto

--- A05Exercicio01/App.js
+++ A05Exercicio01/App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import iconNovo from './assets/icon.png';
+const App = () => {
+  const [nome, setNome] = useState('');
+  const [mensagem, setMensagem] = useState('');
 
-export default function App() {
+  const lidarComClique = () => {
+    if (nome) {
+      setMensagem(`Olá, ' ${nome}`);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.view}>
+        <Image source={iconNovo} style={styles.image} />
+        <Text style={styles.text}>
+          Exemplo de elementos graficos interativos em React Native
+        </Text>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu nome"
+          onChangeText={setNome}
+          value={nome}
+        />
+        <Button title="Enviar" onPress={lidarComClique} />
+      </View>
+
+      {mensagem ? (
+        <View style={styles.messageContainer}>
+          <Text style={styles.message}>{mensagem}</Text>
+        </View>
+      ) : null}
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Botao customizado</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
+  },
+  view: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  messageContainer: {
+    marginBottom: 20,
+  },
+  message: {
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    allignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
+
+export default App;
